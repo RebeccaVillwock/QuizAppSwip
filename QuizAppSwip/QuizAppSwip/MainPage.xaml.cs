2e6add8a49a500ea990dc854e8616e2e8d2c969e
RebeccaVillwock <rvillwock1@my.wctc.edu>
2020-12-05 18:23:55 -0600

    Add project files.

---
+++ QuizAppSwip/QuizAppSwip/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QuizAppSwip
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnSwiped(object sender, SwipedEventArgs e) 
+        {
+            
+        }
+    }
+}
